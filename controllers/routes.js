18bc1f209988398ef85f7337c7a565568f341925
jakekasan <jake.kasan@gmail.com>
2018-06-27 23:42:50 +0200

    altered routing

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,9 +1,22 @@
-module.exports = function(app){
-    app.use("/",(req,res) => {
-        res.send("home");
-    });
+const express = require("express");
+const router = express.Router();
 
-    app.use("/ticket",(req,res) => {
-        res.send("ticket page");
-    });
-}
+
+router.get("/",(req,res) => {
+    res.send("home");
+});
+
+router.get("/ticket",(req,res) => {
+    res.send("ticket page");
+});
+
+router.get("/test",(res,req) => {
+    // this will generate a bunch of test data for demo purposes
+    res.send("Ok!");
+});
+
+router.post("/data",(req,res) => {
+    res.send("Ok!");
+});
+
+module.exports = router;
